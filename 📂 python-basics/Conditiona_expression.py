1c470f35bfa27e5dba01a36ceac5cb9ea5a672fc
Pritam Ghosh <108845696+HITHCHHIKER96@users.noreply.github.com>
2026-05-01 11:37:13 +0530

    Create Conditiona_expression.py

---
+++ 📂 python-basics/Conditiona_expression.py
@@ -0,0 +1,10 @@
+#conditional expression : A one-line shortcut for the if-else statement(ternary operator)
+#                         Print or assign one of two values based on condition
+#                         X if condition else Y
+
+Day = 20
+print(True if Day>=30 else False)
+
+Age = 20
+Access = "Allowed" if Age>18 else "Not Allowed"
+print(Access)
